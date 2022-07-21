fe89b9b7e4d94232509c68249b331187fe02c6f9
Amenable-C <duswo0624@naver.com>
2022-07-22 00:17:39 +0900

    boj-2309

--- 8weeks/1st-week/bj2309.cpp
+++ 8weeks/1st-week/bj2309.cpp
@@ -1,2 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main(){
+	
+	int a[9] = {};
+	int sum = 0;
+	for(int i = 0; i < 9; i++){
+		cin >> a[i];
+		sum = sum + a[i];
+	}
+	
+	
+	int j;
+	int check;
+	for(int i = 0; i < 9; i++){
+		for(j = i + 1; j < 9; j++){
+			check = sum;
+			check = check - a[i] - a[j];
+			if(check == 100)
+				break;
+		}
+		
+		if(check == 100){
+			a[i] = 0;
+			a[j] = 0;
+			
+			sort(a, a + 9);
+			
+			for(int k = 2; k < 9; k++)
+				cout << a[k] << endl;
+			
+			break;
+		}	
+	}
+	
+	
+	return 0;
+	
+}
