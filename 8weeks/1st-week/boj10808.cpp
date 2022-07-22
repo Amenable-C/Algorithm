a00afc2b52da682493fda8e0506fad6750155289
Amenable-C <duswo0624@naver.com>
2022-07-22 09:43:06 +0900

    boj-10808

---
+++ 8weeks/1st-week/boj10808.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[26];
+int main(){
+	string s; 
+	cin >> s;
+		
+	for(int i = 0; i < s.size(); i++)
+		a[s[i]-'a']++;
+
+	for(int i = 0; i < 26; i++)
+		cout << a[i] << " ";
+}
+
+
