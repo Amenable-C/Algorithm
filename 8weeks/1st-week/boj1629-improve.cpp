6b02923a502c6001e43af39ecc6432d3d0e64d84
Amenable-C <duswo0624@naver.com>
2022-07-28 21:24:11 +0900

    boj-1629-improve

---
+++ 8weeks/1st-week/boj1629-improve.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll a, b, c;
+
+ll go(ll a, ll b){
+	if(b == 1) 
+		return a % c;
+	
+	ll _c = go(a, b / 2);
+	_c = (_c * _c) % c;
+	
+	if(b % 2) // b가 홀수일 경우, 하나가 그냥 날라감 // 5 -> 2, 2, 1 // 9 -> 4, 4, 1// 여기서 '1'을 고려하는 것!! 
+		_c = (_c * a ) % c;
+	
+	return _c;
+}
+
+
+int main(){
+	cin >> a >> b >> c;
+	cout << go(a, b);
+	return 0;
+	
+}
