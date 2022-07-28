b71a59aab8ff884220b4c5b497ba902b4663fc14
Amenable-C <duswo0624@naver.com>
2022-07-28 20:36:00 +0900

    boj-3986-improve

---
+++ 8weeks/1st-week/boj3986-improve.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret;
+string s;
+
+int main(){
+	cin >> n;
+	
+	for(int i = 0; i < n; i++){
+		cin >> s;
+		stack<char> stk;
+		
+		for(char c : s){
+			if(stk.size() && stk.top() == c)
+				stk.pop();
+			else
+				stk.push(c);
+		
+		}
+		
+		if(stk.size() == 0) ret++;
+	}
+	
+	cout << ret;
+}
