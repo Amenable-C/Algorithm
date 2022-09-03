f7889fc200411b8c224d23a70e14d8319d957f25
Amenable-C <duswo0624@naver.com>
2022-09-03 13:31:42 +0900

    boj-14620-2

---
+++ 8weeks/3rd-week/boj14620-2.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, ret = 987654321, m[20][20], v[20][20];
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+bool check(int y, int x){
+	if(v[y][x])
+		return false;
+	
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		
+		if(ny < 0 || nx < 0 || ny >= n || nx >= n || v[ny][nx])
+			return false;
+	}
+	return true;
+}
+
+int setFlower(int y, int x){
+	v[y][x] = 1;
+	int s = m[y][x];
+	
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		
+		v[ny][nx] = 1;
+		s += m[ny][nx];
+	}
+	
+	return s;
+}
+
+void eraseFlower(int y, int x){
+	v[y][x] = 0;
+	for(int i = 0; i < 4; i++){
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		v[ny][nx] = 0;
+	}
+}
+
+void flower(int cnt, int sum){
+	if(cnt == 3){
+		ret = min(ret, sum);
+		return;
+	}
+	
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(check(i, j)){
+				flower(cnt + 1, sum + setFlower(i, j));
+				eraseFlower(i, j);	
+			}
+		}
+	}
+}
+
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cin >> m[i][j];	
+		}
+	}
+	
+	flower(0, 0);
+	cout << ret;
+	return 0;
+}
