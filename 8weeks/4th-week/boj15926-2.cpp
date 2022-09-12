ec9e8c7ca76bac3fb635a0e2a5b687849ada6a36
Amenable-C <duswo0624@naver.com>
2022-09-12 12:05:31 +0900

    boj-15926-2

---
+++ 8weeks/4th-week/boj15926-2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, cnt, ret;
+string s;
+stack<int> stk;
+
+int main(){
+	cin >> n >> s;
+	stk.push(-1);
+	for(int i = 0; i < n; i++){
+		if(s[i] == '('){
+			stk.push(i);	
+		}
+		else{
+			stk.pop();
+			if(!stk.empty()){
+				ret = max(ret, i - stk.top());
+			}
+			else{
+				stk.push(i);
+			}
+		}
+	}
+	
+	cout << ret;
+	return 0;
+}
