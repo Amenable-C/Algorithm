d8325d9da70cea1b2f0551dcae1e21d7da72a604
Amenable-C <duswo0624@naver.com>
2022-09-13 15:22:21 +0900

    boj-2109-improve

---
+++ 8weeks/4th-week/boj2109-improve.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a, b, ret;
+vector<pair<int, int>> v;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+	cin >> n;
+	
+	for(int i = 0; i < n; i++){
+		cin >> a >> b;
+		v.push_back({b, a});	
+	}
+	
+	sort(v.begin(), v.end());
+	
+	for(int i = 0; i < n; i++){
+		pq.push(v[i].second);
+		if(pq.size() > v[i].first){ // value가 작은게 pop되는 것. 
+			pq.pop();
+		}
+	}
+	
+	while(pq.size()){
+		ret += pq.top();
+		pq.pop();	
+	}
+	
+	cout << ret;
+	return 0; 
+	
+}
