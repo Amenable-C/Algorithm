7bf419c82220c2c640c3d942b11d65544dcf3f60
Amenable-C <duswo0624@naver.com>
2022-09-13 21:19:25 +0900

    boj-14469

---
+++ 8weeks/4th-week/boj14469.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, a, b, ret;
+vector<pair<int, int>> v;
+
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> a >> b;
+		v.push_back({a, b});
+	}
+	
+	sort(v.begin(), v.end());
+	
+	ret = v[0].first + v[0].second;
+	for(int i = 1; i < n; i++){
+		if(v[i].first > ret)
+			ret = v[i].first + v[i].second;
+		else{
+			ret += v[i].second;	
+		}
+	}
+	cout << ret;
+	return 0;
+}
