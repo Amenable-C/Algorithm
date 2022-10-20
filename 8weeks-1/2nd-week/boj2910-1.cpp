8e1e52eede8a5d79921b07d7dfae76250d82c6b2
Amenable-C <duswo0624@naver.com>
2022-10-20 19:12:59 +0900

    boj-2910-1

---
+++ 8weeks-1/2nd-week/boj2910-1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+ll n, c, num;
+vector<pair<int, int>> v;
+map<int, int> mp, mp_first;
+
+bool cmp(pair<int, int> a, pair<int, int> b){
+	if(a.first == b.first){
+		return mp_first[a.second] < mp_first[b.second];
+	}
+	return a.first > b.first;
+}
+
+
+int main(){
+	cin >> n >> c;
+	for(int i = 0; i < n; i++){
+		cin >> num;
+		mp[num]++;
+		if(mp_first[num] == 0)
+			mp_first[num] = i + 1;
+	}
+	
+	for(auto it : mp){
+		v.push_back({it.second, it.first});	
+	}
+	
+	sort(v.begin(), v.end(), cmp);
+	
+	for(auto it : v){
+		for(int i = 0; i < it.first; i++){
+			cout << it.second << " ";
+		}
+	}
+	return 0;
+}
